00466fc169947a8c712c560a6119a30cccfee6ea
huangjiamin_03 <1976867394@qq.com>
2025-04-02 22:39:27 +0800

    hh

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Moban from './Gp7/Moban';
+import ImageGallery from "./Gp7/Huangjiamin";
 const App = () => {
 
   return (
     <>
-      <Moban></Moban>
+      <ImageGallery></ImageGallery>
     </>
   );
 }
